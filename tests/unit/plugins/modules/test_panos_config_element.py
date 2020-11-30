791d4d2f4a3ab909685359c4b70c66e707ebc39f
Nathan <nembery@gmail.com>
2020-11-30 16:00:50 -0500

    remove conflicts add tests to capture new behavior

--- tests/unit/plugins/modules/test_panos_config_element.py
+++ tests/unit/plugins/modules/test_panos_config_element.py
@@ -4,13 +4,8 @@ __metaclass__ = type
 
 from ansible_collections.mrichardson03.panos.plugins.modules import panos_config_element
 
-<<<<<<< HEAD
-from .common.utils import (
-    ModuleTestCase,
-)
-=======
+
 from .common.utils import ModuleTestCase
->>>>>>> upstream/develop
 
 
 class TestPanosConfigElement(ModuleTestCase):
@@ -18,17 +13,20 @@ class TestPanosConfigElement(ModuleTestCase):
 
     def test_existing_element(self, connection_mock):
 
-<<<<<<< HEAD
-        element = '<some>xml</some>'
-=======
-        element = "<some>xml</some>"
->>>>>>> upstream/develop
-        connection_mock.get.return_value = element
+        element_set = "<some>xml</some>"
 
-        result = self._run_module({"xpath": "/some/xpath", "element": element})
+        # get will return xml wrapped in last node in xpath, along with possibly other items at the same level
+        element_get = "<response><result><xpath><some>xml</some><other_tag></other_tag></xpath></result></response>"
+
+        connection_mock.get.return_value = element_get
+
+        import pydevd_pycharm
+        pydevd_pycharm.settrace('localhost', port=52001, stdoutToServer=True, stderrToServer=True)
+
+        result = self._run_module({"xpath": "/some/xpath", "element": element_set})
 
         assert not result["changed"]
-        assert result["diff"] == {"before": element, "after": element}
+        assert result["diff"] == {"before": element_get, "after": element_set}
 
     def test_changed_element(self, connection_mock):
 
@@ -36,14 +34,11 @@ class TestPanosConfigElement(ModuleTestCase):
         new_element = '<entry name="hi again">more xml</entry>'
 
         connection_mock.get.side_effect = [existing_element, new_element]
-<<<<<<< HEAD
-        connection_mock.set.return_value = 200, '<response status="success" code="20"><result/></response>'
-=======
+
         connection_mock.set.return_value = (
             200,
             '<response status="success" code="20"><result/></response>',
         )
->>>>>>> upstream/develop
 
         result = self._run_module({"xpath": "/some/xpath", "element": new_element})
 
