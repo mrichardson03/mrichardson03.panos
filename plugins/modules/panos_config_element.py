b6183deb0fc4bc4e880e60d31c4eecdc7d28ea82
Michael Richardson <mirichardson@paloaltonetworks.com>
2020-12-22 10:16:03 -0500

    feat(panos_config_element): Support state

--- plugins/modules/panos_config_element.py
+++ plugins/modules/panos_config_element.py
@@ -22,52 +22,80 @@ __metaclass__ = type
 DOCUMENTATION = """
 ---
 module: panos_config_element
-short_description: Sets arbitrary configuration element in the PAN-OS configuration
+short_description: Modifies an element in the PAN-OS configuration.
 description:
-    - This module will allow user to pass an xpath and element to be 'set' in the PAN-OS configuration.
+    - This module allows the user to modify an element in the PAN-OS configuration
+      by specifying an element and its location in the configuration (xpath).
 author:
     - 'Nathan Embery (@nembery)'
+    - 'Michael Richardson (@mrichardson03)'
 version_added: '1.0.0'
 requirements: []
-
 notes:
     - Checkmode is supported.
     - Panorama is supported.
+extends_documentation_fragment:
+    - mrichardson03.panos.fragments.state
 options:
     xpath:
         description:
-            - The xpath address where the XML snippet will be inserted into the PAN-OS configuration
+            - Location of the specified element in the XML configuration.
         type: str
         required: true
     element:
         description:
-            - The XML snippet to be inserted into the PAN-OS configuration
+            - The element, in XML format.
         type: str
-        required: true
     override:
         description:
-            - Override existing configuration elements if True, otherwise, merge with existing configuration elements
+            - If **true**, override any existing configuration at the specified
+              location with the contents of *element*.
+            - If **false**, merge the contents of *element* with any existing
+              configuration at the specified location.
         type: bool
         default: False
         required: false
 """
 
 EXAMPLES = """
-- name: configure login banner
+- name: Configure login banner
   vars:
     banner_text: 'Authorized Personnel Only!'
   panos_config_element:
     xpath: '/config/devices/entry[@name="localhost.localdomain"]/deviceconfig/system'
     element: '<login-banner>{{ banner_text }}</login-banner>'
 
+- name: Create address object
+  panos_config_element:
+    xpath: "/config/devices/entry[@name='localhost.localdomain']/vsys/entry[@name='vsys1']/address"
+    element: |
+      <entry name="Test-One">
+        <ip-netmask>1.1.1.1</ip-netmask>
+      </entry>
+
+- name: Delete address object 'Test-One'
+  panos_config_element:
+    xpath: "/config/devices/entry[@name='localhost.localdomain']/vsys/entry[@name='vsys1']/address/entry[@name='Test-One']"
+    state: 'absent'
 """
 
 RETURN = """
-diff:
-    description: Differences found from existing running configuration and requested element
-    returned: success
+changed:
+    description: A boolean value indicating if the task had to make changes.
+    returned: always
+    type: bool
+msg:
+    description: A string with an error message, if any.
+    returned: failure, always
     type: str
-    sample: {"before": "", "after": ""}
+diff:
+    description:
+        - Information about the differences between the previous and current
+          state.
+        - Contains 'before' and 'after' keys.
+    returned: success, when needed
+    type: dict
+    elements: str
 """
 
 from ansible.module_utils.connection import ConnectionError
@@ -87,10 +115,11 @@ def main():
     module = PanOSAnsibleModule(
         argument_spec=dict(
             xpath=dict(required=True),
-            element=dict(required=True),
+            element=dict(required=False),
             override=dict(type="bool", default=False, required=False),
         ),
         supports_check_mode=True,
+        with_state=True,
     )
 
     if not HAS_LIB:
@@ -98,28 +127,50 @@ def main():
 
     xpath = module.params["xpath"]
     element = module.params["element"]
-
-    override = module.params.get("override", False)
+    override = module.params["override"]
+    state = module.params["state"]
 
     try:
         existing_response = module.connection.get(xpath)
-
         existing_object = xmltodict.parse(existing_response)
 
         existing = existing_object.get("response", {}).get("result", {})
 
-        changed = True
+        changed = False
+        diff = {}
 
-        if __is_present(existing, element) or module.check_mode:
-            changed = False
+        if state == "present":
+            if element is None:
+                module.fail_json(msg="'element' is required when state is 'present'.")
 
-        if changed:
-            if override:
-                module.connection.edit(xpath, element)
+            # Element does not exist as desired, create/edit it.
+            if not __is_present(existing, element):
+                changed = True
+
+                if not module.check_mode:
+                    if override:
+                        module.connection.edit(xpath, element)
+                    else:
+                        module.connection.set(xpath, element)
+
+            # Element exists as desired.
+            diff = {"before": existing_response, "after": element}
+
+        # state == "absent"
+        else:
+
+            # Element exists, delete it.
+            if existing is not {}:
+                changed = True
+
+                if not module.check_mode:
+                    module.connection.delete(xpath)
+
+                diff = {"before": existing_response, "after": ""}
+
+            # Element doesn't exist, nothing needs to be done.
             else:
-                module.connection.set(xpath, element)
-
-        diff = {"before": existing_response, "after": element}
+                diff = {"before": "", "after": ""}
 
         module.exit_json(changed=changed, diff=diff)
 
@@ -129,14 +180,16 @@ def main():
 
 def __is_present(existing, snippet_string):
     """
-    Simple function to check if a snippet is present in the object as returned from the XML API
+    Simple function to check if a snippet is present in the object as returned
+    from the XML API.
 
     :param existing: object as returned from the module.connection.get method
     :param snippet_string: snippet string we want to add
     :return: boolean True if found to be present
     """
 
-    # snippets must not include the surrounding tag info, which means they are not valid XML by themselves
+    # snippets must not include the surrounding tag info, which means they are
+    # not valid XML by themselves
     wrapped_snippet = "<wrapped>" + snippet_string + "</wrapped>"
 
     # if existing object does not exist, then it can't be present
@@ -153,8 +206,10 @@ def __is_present(existing, snippet_string):
 
 def __is_subset(small, large):
     """
-    Compare two items to determine if the 'small' item is contained in the 'large' item
-    based on answer found here:
+    Compare two items to determine if the 'small' item is contained in the
+    'large' item.
+
+    Based on answer found here:
     https://stackoverflow.com/questions/44120874/find-if-a-dict-is-contained-in-another-new-version
 
     :param small: dict, list or str
@@ -164,7 +219,8 @@ def __is_subset(small, large):
     if isinstance(small, dict) and isinstance(large, dict):
         for key in small.keys():
             # don't mind extra items added to the candidate config
-            # in the normal case, element coming from user will not have these attributes. Can possibly skip this check
+            # in the normal case, element coming from user will not have these
+            # attributes. Can possibly skip this check.
             if key in ["@dirtyId", "@admin", "@time"]:
                 continue
 
@@ -195,8 +251,8 @@ def __is_subset(small, large):
         if isinstance(large, str):
             return small == large
         elif isinstance(large, dict):
-            # The candidate config can do fun things like set an attribute called #text in a dict instead
-            # of creating an actual str value
+            # The candidate config can do fun things like set an attribute
+            # called #text in a dict instead of creating an actual str value
             if "#text" in large:
                 return small == large["#text"]
 
@@ -215,10 +271,15 @@ def __is_subset(small, large):
 
 def __unwrap(dict_object):
     """
-    Simple function to return the first item found in a given dictionary that does not start with an '@'.
-    This is used as xmltodict will return an object with several attributes attached such as @count, @total-count
-    etc. The return from the module.connection.get will also 'wrap' the returned items in an extra attribute as well.
-    This roughly correlates to the last node in the xpath
+    Simple function to return the first item found in a given dictionary that
+    does not start with an '@'.
+
+    This is used as xmltodict will return an object with several attributes
+    attached such as @count, @total-count etc. The return from
+    module.connection.get will also 'wrap' the returned items in an extra
+    attribute as well.
+
+    This roughly correlates to the last node in the xpath.
 
     :param dict_object:
     :return:
