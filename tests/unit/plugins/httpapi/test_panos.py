948f67718bb1e8dffbaf7e2cacfcfa45298c3355
Michael Richardson <mirichardson@paloaltonetworks.com>
2020-12-23 16:10:14 -0500

    refactor: httpapi cleanup (#25)
    * Remove login() method
    * Remove JSON leftovers
    * Centralize response validation

--- tests/unit/plugins/httpapi/test_panos.py
+++ tests/unit/plugins/httpapi/test_panos.py
@@ -2,14 +2,15 @@ from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
 
-import json
 from unittest import mock
+from unittest.mock import patch
 
 import pytest
-from ansible.errors import AnsibleConnectionFailure
-from ansible.module_utils.six import BytesIO, StringIO
-from ansible.module_utils.six.moves.urllib.error import HTTPError
+from ansible.module_utils.six import BytesIO
 from ansible_collections.mrichardson03.panos.plugins.httpapi.panos import HttpApi
+from ansible_collections.mrichardson03.panos.plugins.module_utils.panos import (
+    PanOSAPIError,
+)
 
 GOOD_KEYGEN = """
 <response><result><key>foo</key></result></response>
@@ -19,18 +20,13 @@ BAD_KEYGEN = """
 <response><result><msg>Invalid Credential</msg></result></response>
 """
 
-SHOW_SYSTEM_INFO_RESPONSE = """
-<response status=\"success\">
+VERSION = """
+<response status = "success">
     <result>
-        <system>
-            <hostname>PA-VM</hostname>
-            <uptime>0 days, 0:57:35</uptime>
-            <model>PA-VM</model>
-            <serial>015351000039218</serial>
-            <sw-version>10.0.0</sw-version>
-            <uptime>uptime</uptime>
-            <multi-vsys>multivsys</multi-vsys>
-        </system>
+        <sw-version>10.0.2</sw-version>
+        <multi-vsys>off</multi-vsys>
+        <model>PA-VM</model>
+        <serial>007000001222</serial>
     </result>
 </response>
 """
@@ -54,25 +50,26 @@ class TestPanosHttpApi:
         self.connection_mock = mock.Mock()
         self.plugin = FakeHttpApiPlugin(self.connection_mock)
 
-    def test_login_success(self):
-        self.connection_mock.send.return_value = self._connection_response(
-            GOOD_KEYGEN, status=200
-        )
+    @patch.object(HttpApi, "version")
+    @patch.object(HttpApi, "keygen")
+    def test_api_key_generate(self, mock_keygen, mock_version):
+        self.connection_mock.get_option.side_effect = ["USERNAME", "PASSWORD"]
+        mock_version.return_value = {}
+        mock_keygen.return_value = "foo"
 
-        self.plugin.login("USERNAME", "PASSWORD")
+        ret_value = self.plugin.api_key()
 
-        assert self.plugin.api_key() == "foo"
+        assert ret_value == "foo"
 
-    def test_login_fail(self):
-        self.connection_mock.send.return_value = self._connection_response(
-            BAD_KEYGEN, status=401
-        )
+    @patch.object(HttpApi, "version")
+    def test_api_key_specified(self, mock_version):
+        self.plugin.set_option("api_key", "foo")
+        mock_version.return_value = {}
 
-        with pytest.raises(AnsibleConnectionFailure) as res:
-            self.plugin.login("USERNAME", "PASSWORD")
+        ret_value = self.plugin.api_key()
 
-        assert "Invalid credential" in str(res.value)
-        assert self.plugin.api_key() is None
+        assert ret_value == "foo"
+        assert mock_version.call_count == 1
 
     @pytest.mark.parametrize(
         "response,status,expected",
@@ -88,46 +85,46 @@ class TestPanosHttpApi:
 
         assert key == expected
 
-    def test_handle_http_error(self):
-        self.connection_mock.send.side_effect = HTTPError(
-            "http://testhost.com", 500, "", {}, StringIO('{"errorMessage": "ERROR"}')
-        )
+    @patch.object(HttpApi, "api_key")
+    @patch.object(HttpApi, "send_request")
+    def test_version(self, mock_send_request, mock_api_key):
+        mock_send_request.return_value = (200, VERSION)
+        mock_api_key.return_value = "foo"
 
-        code, response = self.plugin.send_request(None, path="/nonexistent")
-        assert code == 500
+        ret_value = self.plugin.version()
 
-    def test_handle_httperror_retry_login(self):
-        response_mock = mock.Mock()
-        type(response_mock).code = mock.PropertyMock(return_value=401)
+        assert ret_value["sw-version"] == "10.0.2"
 
-        with mock.patch.object(HttpApi, "login", return_value=None) as mock_login:
-            retval = self.plugin.handle_httperror(response_mock)
+    @pytest.mark.parametrize(
+        "http_status,http_response",
+        [(200, "<request status='success'></request>")],
+    )
+    def test_validate_response(self, http_status, http_response):
+        response = self.plugin._validate_response(http_status, http_response)
 
-        assert mock_login.call_count == 1
-        assert retval is True
+        assert response == http_response
 
-    def test_handle_httperror_auth_error(self):
-        response_mock = mock.Mock()
-        type(response_mock).code = mock.PropertyMock(return_value=401)
-        type(self.connection_mock)._auth = mock.PropertyMock(return_value=None)
+    @pytest.mark.parametrize("http_status,http_response", [(401, None)])
+    def test_validate_response_connection_error(self, http_status, http_response):
+        with pytest.raises(ConnectionError):
+            self.plugin._validate_response(http_status, http_response)
 
-        with mock.patch.object(HttpApi, "login", return_value=None) as mock_login:
-            retval = self.plugin.handle_httperror(response_mock)
-
-        assert mock_login.call_count == 0
-        assert retval is False
-
-    def test_handle_httperror_404(self):
-        response_mock = mock.Mock()
-        type(response_mock).code = mock.PropertyMock(return_value=404)
-
-        assert self.plugin.handle_httperror(response_mock) is False
+    @pytest.mark.parametrize(
+        "http_status,http_response",
+        [
+            (200, "<request status='error' code='1'></request>"),
+            (403, "<request status='error' code='403'></request>"),
+        ],
+    )
+    def test_validate_response_api_exception(self, http_status, http_response):
+        with pytest.raises(PanOSAPIError):
+            self.plugin._validate_response(http_status, http_response)
 
     @staticmethod
     def _connection_response(response, status=200):
         response_mock = mock.Mock()
         response_mock.getcode.return_value = status
-        response_text = json.dumps(response) if type(response) is dict else response
+        response_text = response
         response_data = BytesIO(
             response_text.encode() if response_text else "".encode()
         )
