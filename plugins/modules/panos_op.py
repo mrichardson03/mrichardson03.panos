4dbb342f83085b0cdd8f50040c0de1d70ad10ae2
Michael Richardson <mirichardson@paloaltonetworks.com>
2021-05-04 11:34:30 -0400

    feat: Remove job retrieval, will do elsewhere

--- plugins/modules/panos_op.py
+++ plugins/modules/panos_op.py
@@ -38,25 +38,13 @@ options:
         description:
             - Execute this string directly as an operational command.
         type: str
+        required: true
     cmd_is_xml:
         description:
             - If true, treat the cmd option as a string already in XML format.
             - If false, attempt to convert it to XML before execution.
         type: bool
         default: false
-    job_id:
-        description:
-            - Retrieve the status of a specific PAN-OS job.
-            - This runs 'show jobs id <job_id>'.
-        type: int
-    job_type:
-        description:
-            - Retrieve the status of a particular type of PAN-OS job.  If there
-              are multiple jobs of that type, the most recent one is returned.
-            - This runs 'show jobs all', and returns the last job of the
-              requested type.
-        type: str
-        choices: ['AutoCom']
 """
 
 EXAMPLES = """
@@ -68,14 +56,6 @@ EXAMPLES = """
   panos_op:
     cmd: '<show><system><info/></system></show>'
     cmd_is_xml: true
-
-- name: Retrieve status of job id 2
-  panos_op:
-    job_id: 2
-
-- name: Retrieve status of autocommit job
-  panos_op:
-    job_type: 'AutoCom'
 """
 
 RETURN = """
@@ -101,34 +81,7 @@ stdout_dict:
                 }
             }
         }
-
-job:
-    description: Dictionary containing info on the requested job.
-    returned: When either 'job_id' or 'job_type' is specified.
-    type: dict
-    sample: >
-        {
-            "details": {
-                "line": [
-                    "Configuration committed successfully",
-                    "Successfully committed last configuration"
-                ]
-            },
-            "id": "1",
-            "positionInQ": "0",
-            "progress": "100",
-            "queued": "NO",
-            "result": "OK",
-            "status": "FIN",
-            "stoppable": "no",
-            "tdeq": "06:52:58",
-            "tenq": "2021/05/03 06:52:58",
-            "tfin": "06:53:27",
-            "type": "AutoCom"
-        }
 """
-
-import json
 
 try:
     import xmltodict
@@ -163,7 +116,6 @@ def execute_op_cmd(conn, cmd, cmd_is_xml):
 
     xml_output = conn.op(cmd, is_xml=cmd_is_xml)
     obj_dict = xmltodict.parse(xml_output)
-    json_output = json.dumps(obj_dict)
 
     return {"changed": changed, "stdout": xml_output, "stdout_dict": obj_dict}
 
@@ -212,13 +164,10 @@ def check_job_type(conn, job_type):
 def main():
     module = PanOSAnsibleModule(
         argument_spec=dict(
-            cmd=dict(type="str"),
+            cmd=dict(type="str", required=True),
             cmd_is_xml=dict(default=False, type="bool"),
-            job_id=dict(type="int"),
-            job_type=dict(type="str", choices=["AutoCom"]),
         ),
         supports_check_mode=False,
-        required_one_of=[("cmd", "job_id", "job_type")],
     )
 
     if not HAS_LIB:  # pragma: no cover
@@ -226,19 +175,11 @@ def main():
 
     cmd = module.params["cmd"]
     cmd_is_xml = module.params["cmd_is_xml"]
-    job_id = module.params["job_id"]
-    job_type = module.params["job_type"]
 
     results = None
 
     try:
-        if cmd:
-            results = execute_op_cmd(module.connection, cmd, cmd_is_xml)
-        elif job_id:
-            results = show_job_id(module.connection, job_id)
-        else:
-            results = check_job_type(module.connection, job_type)
-
+        results = execute_op_cmd(module.connection, cmd, cmd_is_xml)
         module.exit_json(**results)
     except ConnectionError as e:  # pragma: no cover
         module.fail_json(msg="{0}".format(e))
