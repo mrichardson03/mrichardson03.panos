031228835ae430085b41871709666401cd1f4ab9
Michael Richardson <mirichardson@paloaltonetworks.com>
2021-05-04 11:44:43 -0400

    test: Add get_nested_key tests

--- tests/unit/plugins/module_utils/test_panos.py
+++ tests/unit/plugins/module_utils/test_panos.py
@@ -3,7 +3,11 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 
-from ansible_collections.mrichardson03.panos.plugins.module_utils.panos import cmd_xml
+import pytest
+from ansible_collections.mrichardson03.panos.plugins.module_utils.panos import (
+    cmd_xml,
+    get_nested_key,
+)
 
 
 def test_cmd_xml():
@@ -12,3 +16,12 @@ def test_cmd_xml():
 
     assert cmd_xml("show system info") == show_system_info
     assert cmd_xml('show interface "all"') == show_interface_all
+
+
+@pytest.mark.parametrize(
+    "key_list,expected", [(["one", "two", "three"], "four"), (["five"], None)]
+)
+def test_get_nested_key(key_list, expected):
+    d = {"one": {"two": {"three": "four"}}}
+
+    assert get_nested_key(d, key_list) == expected
